03d3903d338ffe1aaa7db11a79c006129c9afcfd
duniv030 <duniv030@duniv030-PC>
2016-08-17 18:51:59 +0900

    안녕하세요~~~~

--- JavaGame/src/Game.java
+++ JavaGame/src/Game.java
@@ -5,6 +5,10 @@ public class Game {
 		System.out.println("게임프로젝트 깃허브 시작");
 		System.out.println("두번째 테스트 파일 옮림.");
 		System.out.println("세번째");
+		
+		System.out.println("안녕하세요~");
+		System.out.println("안녕하세요~");
+		System.out.println("안녕하세요~");
 	}
 
 }
