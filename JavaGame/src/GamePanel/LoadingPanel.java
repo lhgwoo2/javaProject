f6266f478c8da8852fe39ecc4a222f8c0b1ed850
annayoo <dahiyu@naver.com>
2016-08-29 09:39:47 +0900

    Update LoadingPanel.java

--- JavaGame/src/GamePanel/LoadingPanel.java
+++ JavaGame/src/GamePanel/LoadingPanel.java
@@ -1,60 +1,77 @@
 package GamePanel;
 
-import java.awt.AlphaComposite;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.net.URL;
+import java.io.IOException;
+import java.io.InputStream;
 
-import javax.swing.BoxLayout;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class LoadingPanel extends JPanel {
-
-	Image image;
-
-	// Image splashImage;
-
-	public LoadingPanel() {
-		super();
-	}
-
-	public JPanel loadingPanel() {
-		
-		BoxLayout layoutMgr = new BoxLayout(this, BoxLayout.PAGE_AXIS);
-	    this.setLayout(layoutMgr);
-
-	    URL loadingImage = this.getClass().getResource("/ImagePack/ripple.gif");
-		/*try {
-			bimg = ImageIO.read(is);
-			System.out.println("메인화면 로드 성공");
-		} catch (IOException e) {
-			System.out.println("메인화면 로드 실패");
-			e.printStackTrace();
-		}*/
-	    ImageIcon imageIcon = new ImageIcon(loadingImage);
-	    image = imageIcon.getImage();
-	    JLabel iconLabel = new JLabel();
-	    iconLabel.setIcon(imageIcon);
-	    imageIcon.setImageObserver(iconLabel);
-	    JLabel label = new JLabel("Loading...");
-	    this.add(iconLabel);
-	    this.add(label);
-
-	    return this;
-	}
-
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		Graphics2D g2d = (Graphics2D)g;
-		AlphaComposite alpha;
-		alpha = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f);
-		g2d.setComposite(alpha);
-		g2d.drawImage(image,0,0,200,200,this);
+public class LoadingPanel extends JPanel{
 	
+	Image loadingIcon;
+	Image bg;
+	Image sprite;
+	int sx1, sy1, sx2, sy2;
+	int row, col;
+	
+	
+	public LoadingPanel(){
+		
+		//loadingIcon = Toolkit.getDefaultToolkit().createImage("C:/Users/duniv026/Desktop/ball.gif");
+		InputStream in = getClass().getResourceAsStream("LoadingBG.png");
+		
+		InputStream ball = getClass().getResourceAsStream("ball_bounce.png");
+		setLayout(null);
+		setBounds(500,500,0,0);
+		
+		try {
+			bg = ImageIO.read(in);
+			sprite = ImageIO.read(ball);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//ImageIcon imageIcon = new ImageIcon(ball);
+		JLabel label = new JLabel();
+		label.setText("Loading...");
+		this.add(label);
 		
 	}
+	
+	public void loop() {
+		sx1 = col++ * 50;
+		sy1 = row * 50;
+		sx2 = sx1 + 50;
+		sy2 = sy1 + 50;
+
+		if (col == 6) {
+			row++;
+			col = 0;
+		}
+		if (row == 1) {
+			row = 0;
+		}
+	}
+	
+	
+	@Override
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.drawImage(bg, 0, 0, 1600, 900, 50, 50, 1000, 500, null);
+		g2d.drawImage(sprite, 0, 0, 100, 100, sx1, sy1, sx2, sy2, null);
+
+		/*
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		if(loadingIcon!=null){
+			g.drawImage(loadingIcon, 700, 350, this);
+		}
+*/
+		
+	}
+
 }
